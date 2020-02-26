cea837dc11dda240176cb804ce69a8ebc66fabe7
rinarive <rinarive@gmail.com>
2020-02-26 13:17:34 -0300

    ordeno folder for components

---
+++ burgerqueen/src/components/NameNave.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+
+class NameNave extends Component{
+
+    render (){
+    
+        return(
+            <div className= 'Name'>
+                <h1>aqui va el nombre del cliente</h1>
+                <imput type>cliente</imput>
+            </div>
+        );
+    }
+}
+
+export default NameNave
