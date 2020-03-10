8964598e65b6580ab0dc9cf0520287365dad6960
rinarive <rinarive@gmail.com>
2020-03-10 10:28:17 -0300

    actulizando

--- src/components/formulario.js
+++ src/components/formulario.js
@@ -21,12 +21,9 @@ const Formulario = (props) => {
     }
 
     return (
-        <Fragment>
-            <ul>
-                <h1>Bienvenido... {datos.nombre} <br></br>a Burger Queen</h1>
-
-            </ul>
+        <Fragment>        
             <headers className="flex-column">
+                <h1>Bienvenido... {datos.nombre} <br></br>a Burger Queen</h1>
             <form className="row" onSubmit={enviarDatos}>
                 <div className="col-md-3">
                     <input type="name" placeholder="Nombre" className="form-control" onChange={handleInputChange} name="nombre"></input>
@@ -38,6 +35,7 @@ const Formulario = (props) => {
                 <button type="submit" onClick={props.addTrip} className="btn btn-primary">Enviar</button>
             </form>
             </headers>
+        
         </Fragment>
     );
 }
