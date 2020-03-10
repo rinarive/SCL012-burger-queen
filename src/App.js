cfe02a1cae715ee44c3e03142494bd1b50066b99
fer <fernanda.1991.alejandra@gmail.com>
2020-03-10 10:47:45 -0300

    Actualizado

--- src/App.js
+++ src/App.js
@@ -1,20 +1,9 @@
 import React, { Component } from 'react';
 import './assets/css/App.css';
 
-<<<<<<< HEAD
-
-// componentes
-import Saludo from './components/saludo';
-import ButtonsRol from './components/buttons';
-import Formulario from './components/modal';
-
-
-
-=======
 // componentes
 import ButtonsRol from './components/buttonsRol';
 import Formulario from './components/formulario';
->>>>>>> 2709e779dde7490b5f6bf840b9e6ebd7c2114680
 
 class App extends Component {
 
@@ -39,22 +28,12 @@ class App extends Component {
         <header className="App-header">
        
 
-<<<<<<< HEAD
-              {this.state.isAddTripState && <Formulario />}
-            </div>
-        
-        </header>
-        <div >
-        
-        </div>
-=======
             {this.state.isEmptyState && <Formulario addTrip={this.triggerAddTripState} />}
             
          
             {this.state.isAddTripState && <ButtonsRol />}
 
 
->>>>>>> 2709e779dde7490b5f6bf840b9e6ebd7c2114680
 
         </header>
        
