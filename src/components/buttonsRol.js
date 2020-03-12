439f9e3d5c08bc75b30d10b4fb925a8c5ed4d65f
rinarive <56647000+rinarive@users.noreply.github.com>
2020-03-12 19:15:52 -0300

    actualizado y funcionando en un 50% (#12)
    * agrego stilos a seccion waiters (#4)
    
    * actualizado y funcionando en un 50%

--- src/components/buttonsRol.js
+++ src/components/buttonsRol.js
@@ -39,11 +39,9 @@ class ButtonsRol extends Component {
     render() {
 
         return (
-<<<<<<< HEAD
+
             <Fragment>
-=======
-            <Fragment >
->>>>>>> 32c437e963f7f72b04900c4694196d88b2bdecff
+
                 <Row>
 
                 {this.state.isEmptyState && <ButtonGene title='Mesero' onClick={this.changeAddTripState} />}
